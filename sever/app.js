56c778345d27bdc630911803883a64951a461f67
ShanatoYuuji <c1273099082@qq.com>
2017-01-17 22:32:12 +0900

    修改

--- sever/app.js
+++ sever/app.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function(req, res){
   res.writeHead(200,{'Content-Type': 'text/html'});
   res.write('</pre><h1>shiori</h1><pre>');
-  res.end('shioris Homepage)
+  res.end('shioris Homepage');
 }).listen(80);
 console.log("http server is listening at port 80.");
