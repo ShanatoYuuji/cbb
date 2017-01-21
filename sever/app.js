7fda8cf1543aec9307c2d1142d4a3f1c230d308d
ShanatoYuuji <c1273099082@qq.com>
2017-01-22 01:04:36 +0900

    网站node部署。

--- sever/app.js
+++ sever/app.js
@@ -1,7 +1,83 @@
 var http = require('http');
-http.createServer(function(req, res){
-  res.writeHead(200,{'Content-Type': 'text/html'});
-  res.write('</pre><h1>shiori</h1><pre>');
-  res.end('shioris Homepage');
+var url = require('url');
+var fs = require('fs');
+var path = require('path')
+
+var MIME = require('./MIME.json');
+var onFiles = function(req, res) {
+	var pathname = url.parse(req.url).pathname;
+	pathname = path.normalize(pathname.replace("\.\.", ""));
+	if(pathname === "\\") {
+		pathname = "/webpage/home.html";
+	}
+	//这里可以控制文件的路径  文件对于此js的路径
+	var filepath = "\.\."+pathname;
+	fs.readFile(filepath, "binary", function(err, file) {
+		if(err) {
+			console.log(filepath);
+			console.log("路径出错");
+			res.writeHead(200, {
+				'Content-Type': 'text/javascript;charset=UTF-8'
+			});
+			res.write(pathname);
+			res.end("\n没有这个地址");
+		} else {
+			var ext = path.extname(filepath);
+			ext = ext ? ext.slice(1) : 'unknown';
+			//文件类型
+			var contentType = MIME[ext] || "application/octet-stream";
+			console.log(contentType);
+			res.writeHead(200, {
+				'Content-Type': contentType
+			});
+			res.write(file, "binary");
+			res.end();
+		}
+
+	})
+}
+
+//允许访问网页
+var webpath = {
+	"/": "/webpage/home.html",
+	"/about": "/webpage/home.html",
+	"/page1": "/pages/homework01.html",
+	"/page2": "/pages/homework02.html",
+	"/page3": "/pages/homework03.html"
+}
+
+//内容返回
+var on202 = function(req, res, bodyStr) {
+	//设置为UTF-8
+	res.writeHead(200, {
+		'Content-Type': 'text/javascript;charset=UTF-8'
+	});
+	res.end(bodyStr);
+}
+
+//网页返回
+var on200 = function(req, res, pathname) {
+	var pathname = ".." + pathname;
+	fs.readFile(pathname, "utf-8", function(err, file) {
+		res.write(file, "utf-8");
+		res.end();
+	});
+};
+
+http.createServer(function(req, res) {
+//		var pathname = url.parse(req.url).pathname;
+//		var bodyStr = webpath[pathname];
+//		if(bodyStr) {
+//			//on202(req, res, bodyStr);
+//			on200(req, res,bodyStr);
+//		} else {
+//			res.writeHead(200, {
+//				'Content-Type': 'text/javascript;charset=UTF-8'
+//			});
+//			res.write(pathname);
+//			res.end("\n没有这个地址");
+//		}
+	onFiles(req, res);
+
 }).listen(80);
 console.log("http server is listening at port 80.");
